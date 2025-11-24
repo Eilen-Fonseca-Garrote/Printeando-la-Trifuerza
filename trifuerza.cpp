3bda67352edea368ab3abb117daa7f1f8b6ecbb9
Eilen Fonseca Garrote <laneko27@gmail.com>
2025-11-24 01:14:09 -0500

    Crear trifuerza.cpp

---
+++ trifuerza.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+//#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+    int numberOfRows = 10;
+
+
+    char spaces = '.';
+    char blocks = '#';
+
+    // Punta del triángulo
+
+    for (int row = 1; row <= numberOfRows; row++) {
+        for (int j = 0; j < numberOfRows * 2 - row; j++)
+        {
+            cout << spaces;
+        }
+        for (int j = 0; j < row * 2 - 1; j++)
+        {
+            cout << blocks;
+        }
+        for (int j = 0; j < numberOfRows * 2 - row; j++)
+        {
+            cout << spaces;
+        }
+        cout << endl;
+    }
+    // resto del triángulo
+
+    for (int row = 1; row <= numberOfRows; row++) {
+        for (int j = 0; j < numberOfRows - row; j++)
+        {
+            cout << spaces;
+        }
+        for (int j = 0; j < row * 2 - 1; j++)
+        {
+            cout << blocks;
+        }
+        for (int j = 0; j < (numberOfRows - row) * 2 + 1; j++)
+        {
+            cout << spaces;
+        }
+        for (int j = 0; j < row * 2 - 1; j++)
+        {
+            cout << blocks;
+        }
+        for (int j = 0; j < numberOfRows - row; j++)
+        {
+            cout << spaces;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
